4f28d1ad6f9f8b8b86b4114974429f7820e08ff0
KhaLe <kha.le@mtech.vn>
2016-12-27 11:46:48 +0700

    create server

--- 11.Clients & Servers/app.js
+++ 11.Clients & Servers/app.js
@@ -1,10 +1,11 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
+  console.log('request was made at route: ' + req.url);
   res.writeHead(200, {"Content-Type":"text/plain"});
   res.end('Hey server was started');
 });
 
-server.listen(7777);
+server.listen(7777, '127.0.0.1');
 
 console.log('Server start at localhost: 7777');
