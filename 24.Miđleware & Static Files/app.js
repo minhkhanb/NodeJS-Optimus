5afc122b32f65bff04e390f2c25a38c0f3daf44e
KhaLe <kha.le@mtech.vn>
2016-12-27 16:04:56 +0700

    Template engine

---
+++ 24.Miđleware & Static Files/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+//app.use('views', './views');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/contact', function (req, res) {
+  res.render('contact');
+});
+
+app.get('/profile/:name', function (req, res) {
+  var data = {
+    age: 27,
+    job: 'developer',
+    hobbies: ['eating', 'fighting', 'fishing']
+  };
+  res.render('profile', {person: req.params.name, data: data});
+});
+
+app.listen(7777);
+console.log('Server started at localhost:7777');
