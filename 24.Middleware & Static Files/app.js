96c091424a10d15dd9529344a2b68ab76c60ba67
KhaLe <kha.le@mtech.vn>
2016-12-27 16:28:45 +0700

    middleware & static file

--- 24.Middleware & Static Files/app.js
+++ 24.Middleware & Static Files/app.js
@@ -3,6 +3,12 @@ var app = express();
 
 app.set('view engine', 'ejs');
 //app.use('views', './views');
+//attach middleware
+app.use('/assets', express.static('assets'));
+// app.use('/assets', function (req, res, next) {
+//   console.log(req.url);
+//   next();
+// });
 
 app.get('/', function (req, res) {
   res.render('index');
