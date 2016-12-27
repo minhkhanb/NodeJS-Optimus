fd06aff633ff0f58e20d186990557f59a18a62cc
KhaLe <kha.le@mtech.vn>
2016-12-27 15:27:57 +0700

    Route Params

--- 21.Express Route Params/app.js
+++ 21.Express Route Params/app.js
@@ -9,4 +9,10 @@ app.get('/contact', function (req, res) {
   res.send('this is the contact page');
 });
 
+app.get('/profile/:id', function (req, res) {
+
+  res.send('You\'re requested to see a profile with the id of ' + req.params.id);
+});
+
 app.listen(7777);
+console.log('Server started at localhost:7777');
